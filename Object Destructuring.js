0c84e088db80b01e32b4fc607782fcc9b03ee2c7
Okasha Khan <71649242+okashakhan4141@users.noreply.github.com>
2021-09-06 15:27:09 +0500

    Destructuring Objects

---
+++ Object Destructuring.js
@@ -0,0 +1,65 @@
+`use strict`
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: ['Via Angelo Tavanti 23, Firenze, Italy'],
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  // ES6 object literals
+  openingHours,
+
+};
+
+
+const { name, categories } = restaurant;
+console.log(name, categories);
+
+//changing names
+const {
+  name: restaurantName,
+  categories: tags,
+} = restaurant;
+
+console.log(restaurantName, tags);
+
+// Default values + Alias
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+// since a and b are already defined so enclosing them in ()
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested objects
+const { fri: { open: o, close: c }} = openingHours;
+console.log(o, c);
+
+//Nested onjects (another way)
+const {fri: friday} = openingHours;
+console.log(friday);
+
+const {open, close} = friday;
+console.log(open, close);
