7762bc656082d7e3420e1d7a97ccab4c949b5f87
Okasha Khan <71649242+okashakhan4141@users.noreply.github.com>
2021-09-06 18:05:21 +0500

    Update Call, Apply & Bind.js

--- Call, Apply & Bind.js
+++ Call, Apply & Bind.js
@@ -16,10 +16,15 @@ const obj2 = {
 //call and apply will bind the function with the object that is being passed as a first argument
 //the only diff b/w call and apply is that the way of passing other arguments (other than this) for a function  call
 
+// way 01
 console.log(add.call(obj1, 2, 3));
 console.log(add.apply(obj2, [2, 3]));
 
-//the bind will bind the obj with function but instead of direct calling, it will return a binded function for later use
+// way 02
+const obj1Fun = add;
+console.log(obj1Fun.call(obj1, 2, 3));
 
+
+//the bind will bind the obj with function but instead of direct calling, it will return a binded function for later use
 const addObj1 = add.bind(obj1);
 console.log(addObj1(2, 5));
