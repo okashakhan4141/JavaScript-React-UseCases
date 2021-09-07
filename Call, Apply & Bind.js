6a5f1f64dff2d8ee97b726486b92ce69b8594434
Mubeen Khan <63047730+i170071MubeenKhan@users.noreply.github.com>
2021-09-07 11:54:14 +0500

    Update Call, Apply & Bind.js

--- Call, Apply & Bind.js
+++ Call, Apply & Bind.js
@@ -13,8 +13,10 @@ const obj2 = {
 	value: 2
 }
 
-//call and apply will bind the function with the object that is being passed as a first argument
+//call and apply will bind the function with the object that is being passed as a first argument - function borrowing concept
 //the only diff b/w call and apply is that the way of passing other arguments (other than this) for a function  call
+// for call -> arguments are passed comma seperated
+// for apply -> arguments are passed as a list (not 'this')
 
 // way 01
 console.log(add.call(obj1, 2, 3));
@@ -25,6 +27,6 @@ const obj1Fun = add;
 console.log(obj1Fun.call(obj1, 2, 3));
 
 
-//the bind will bind the obj with function but instead of direct calling, it will return a binded function for later use
+//the bind will bind the obj with function but instead of direct calling, it will return a binded function for later use - makes a function copy
 const addObj1 = add.bind(obj1);
 console.log(addObj1(2, 5));
