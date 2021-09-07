17d38e937b30a7675b9040149ad60129f386f135
Mubeen Khan <63047730+i170071MubeenKhan@users.noreply.github.com>
2021-09-07 14:35:51 +0500

    Update Object Destructuring.js

--- Object Destructuring.js
+++ Object Destructuring.js
@@ -66,3 +66,25 @@ console.log(friday);
 
 const {open, close} = friday;
 console.log(open, close);
+
+
+// another example
+// object
+const person = {
+  name: 'Mubeen',
+  age: 22,
+  func() {
+    console.log("person.func has been called!", this);
+    console.log('\n');
+  }
+};
+
+//const name = person.name;
+//const age = person.age
+
+//const { name, age } = person; // same as above
+
+const { name: nm, age: a } = person;  // person's name in nm and age in a
+console.log(nm, a);
+
+// Output: Mubeen 22
