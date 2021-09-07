c348e725b1c12c48de11bec12149acb78c4e047f
Mubeen Khan <63047730+i170071MubeenKhan@users.noreply.github.com>
2021-09-07 13:15:08 +0500

    Update MAP.js

--- MAP.js
+++ MAP.js
@@ -26,8 +26,19 @@ rest.delete(2);
 
 const arr = [1, 2];
 rest.set(arr, 'Test');
-rest.set(document.querySelector('h1'), 'Heading');
 console.log(rest);
 console.log(rest.size);
 
 console.log(rest.get(arr));
+
+/* Output:
+Map(3) {'name' => 'Classico Italiano', 1 => 'Firenze, Italy', 2 => 'Lisbon, Portugal'}
+Classico Italiano
+(4) ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']
+Firenze, Italy
+We are closed :(
+true
+Map(8) {'name' => 'Classico Italiano', 1 => 'Firenze, Italy', 'categories' => Array(4), 'open' => 11, 'close' => 23, …}
+8
+Test
+*/
