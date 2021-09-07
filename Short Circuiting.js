48e4b1a7531e1627868347655093d0259b4d4c93
Okasha Khan <71649242+okashakhan4141@users.noreply.github.com>
2021-09-07 11:00:46 +0500

    Short Circuiting (&& and ||)

---
+++ Short Circuiting.js
@@ -0,0 +1,26 @@
+`use strict`
+
+
+// Short Circuiting (&& and ||)
+
+console.log('---- OR ----');
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+let numGuests = 0;
+const guests1 = numGuests ? numGuests : 10;
+console.log(guests1);
+
+const guests2 = numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'jonas');
