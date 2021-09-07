1414d176e0ac43980fb8437c483726cc0669d5f2
Okasha Khan <71649242+okashakhan4141@users.noreply.github.com>
2021-09-07 14:42:56 +0500

    Nullish Operator

--- Nullish.js
+++ Nullish.js
@@ -4,11 +4,11 @@
 // undefined , null , NaN , 0 , "" and false
 
 // The Nullish Coalescing Operator
-const number = null
+const number = 0
 let result = number || 10;
 console.log(result);
 
 // Nullish: null and undefined (NOT 0 or '')
-// only `null` and `undefined` will give 10
+// only `null` and `undefined` will give 10 i.e: false
 result = number ?? 10;
 console.log(result);
