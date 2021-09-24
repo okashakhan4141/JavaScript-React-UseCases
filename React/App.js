c347464e76438e2d20f37603fb53fd3b2ba0788c
hamzaayad <60011170+hamzaayad@users.noreply.github.com>
2021-09-24 15:38:41 +0500

    Add files via upload

---
+++ React/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import NameAge from "./NameAge";
+
+function App() {
+  return (
+    <div className="App">
+      <NameAge name="Hamza Ayad" age="21"></NameAge>
+      <NameAge name="Okasha Khan" age="22"></NameAge>
+      <NameAge name="Farhan Ali" age="22"></NameAge>
+      <NameAge name="Sara Ashraf" age="23"></NameAge>
+    </div>
+  );
+}
+
+export default App;
